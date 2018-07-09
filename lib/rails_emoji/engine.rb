dc45b5031c0d175fbe172dd5c8a62543b36bd24d
Filipe Pina <filipe.c.pina@gmail.com>
2018-07-09 02:27:30 +0100

    added emojis to asset pipeline

--- lib/rails_emoji/engine.rb
+++ lib/rails_emoji/engine.rb
@@ -1,4 +1,7 @@
 module RailsEmoji
   class Engine < ::Rails::Engine
+    initializer 'rails_emoji.assets.precompile' do |app|
+      app.config.assets.precompile << %r(emojis\/.*\.(?:png|svg|gif)$)
+    end
   end
 end
